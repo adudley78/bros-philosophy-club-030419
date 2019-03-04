4286de7fa1346796ef66a17ad1f3ef89641e20ec
adudley78 <dudleyadam@gmail.com>
2019-03-04 14:46:20 -0500

    Build static controller and static#about

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,8 @@
+class StaticController < ApplicationController
+
+  def show
+
+    render :about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+   get "/static/:about" => "static#show", as: 'about'
+
   root 'welcome#index'
 
 end
